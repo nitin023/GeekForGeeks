9e506d7b5a88c0233054640021ddb25cd8f6ddae
nitin023 <khulbe.nitin@gmail.com>
2020-01-23 21:34:25 +0530

    added code for reversing string without extra space

---
+++ src/LeetCode/Easy/Strings/ReverseString.java
@@ -0,0 +1,35 @@
+package LeetCode.Easy.Strings;
+
+/**
+ * Write a function that reverses a string. The input string is given as an array of characters char[].
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * You may assume all the characters consist of printable ascii characters.
+ *
+ * Example 1:
+ *
+ * Input: ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ *
+ * Input: ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ */
+public class ReverseString {
+
+    public static void main(String[] args) {
+        reverseString(new char[]{'h','e','l','l','o'});
+    }
+
+    public static void reverseString(char[] s) {
+
+        int startIndex = 0 , endIndex = s.length - 1;
+        for(startIndex = 0 ; startIndex<endIndex; startIndex++ ,endIndex--)
+        {
+            char startChar = s[startIndex];
+            s[startIndex] = s[endIndex];
+            s[endIndex] = startChar;
+        }
+    }
+}
