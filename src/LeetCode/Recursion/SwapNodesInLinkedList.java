08d209450cd79f0227dd1c236622f21e55f8701a
nitinFareye1 <nitin.khulbe@getfareye.com>
2020-03-30 17:59:03 +0000

    Done with pairwise swapping of adjacent nodes using recursion

--- src/LeetCode/Recursion/SwapNodesInLinkedList.java
+++ src/LeetCode/Recursion/SwapNodesInLinkedList.java
@@ -1,17 +1,15 @@
 package LeetCode.Recursion;
 
 /**
- *
  * Swap Nodes in Pairs
- *
+ * <p>
  * Given a linked list, swap every two adjacent nodes and return its head.
- *
+ * <p>
  * You may not modify the values in the list's nodes, only nodes itself may be changed.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Given 1->2->3->4, you should return the list as 2->1->4->3.
- *
  */
 public class SwapNodesInLinkedList {
 
@@ -21,41 +19,33 @@ public class SwapNodesInLinkedList {
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next = new ListNode(6);
 
-        ListNode newHead  = swapPairsIteratively(head);
+        ListNode newHead = swapPairsRecursively(head, null, null);
+        System.out.println(1);
     }
 
     public static ListNode swapPairsIteratively(ListNode head) {
 
-        if(head == null)
-        {
+        if (head == null) {
             return null;
-        }
-
-        else if(head.next == null)
-        {
+        } else if (head.next == null) {
             return head;
         }
 
-        ListNode prevNode = head , nextNode ,nextNodeNext , tempHead = null ,tempConnector = null;
+        ListNode prevNode = head, nextNode, nextNodeNext, tempHead = null, tempConnector = null;
 
-        while (prevNode !=null && prevNode.next !=null)
-        {
+        while (prevNode != null && prevNode.next != null) {
             nextNode = prevNode.next;
             nextNodeNext = nextNode.next;
 
             //swap prev/next node
             nextNode.next = prevNode;
 
-            if(tempConnector !=null)
-            {
+            if (tempConnector != null) {
                 tempConnector.next = nextNode;
             }
 
-            if(tempHead == null)
-            {
+            if (tempHead == null) {
                 tempHead = nextNode;
             }
             prevNode.next = nextNodeNext;
@@ -65,5 +55,41 @@ public class SwapNodesInLinkedList {
 
         head = tempHead;
         return head;
+    }
+
+    /**
+     * Just converted the iterative loop to recursion call and we are done with recursive soln
+     *
+     * @param prevNode
+     * @param tempHead
+     * @param tempConnector
+     * @return
+     */
+    public static ListNode swapPairsRecursively(ListNode prevNode, ListNode tempHead, ListNode tempConnector) {
+
+        ListNode nextNode, nextNodeNext;
+
+        if (prevNode == null || prevNode.next == null) {
+            return null;
+        }
+        nextNode = prevNode.next;
+
+        nextNodeNext = nextNode.next;
+
+        nextNode.next = prevNode;
+        if (tempConnector != null) {
+            tempConnector.next = nextNode;
+        }
+
+        if (tempHead == null) {
+            tempHead = nextNode;
+        }
+
+        prevNode.next = nextNodeNext;
+        tempConnector = prevNode;
+        prevNode = nextNodeNext;
+
+        swapPairsRecursively(prevNode, tempHead, tempConnector);
+        return tempHead;
     }
 }
