ee0e02acbdc6d3e7463d337c5ff7bcc58843ab7c
nitinFareye1 <nitin.khulbe@getfareye.com>
2020-04-25 09:29:32 +0000

    Target sum solved using recursion with memorosation

---
+++ src/LeetCode/QueueAndStack/TargetSum.java
@@ -0,0 +1,62 @@
+package LeetCode.QueueAndStack;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+ * <p>
+ * Find out how many ways to assign symbols to make sum of integers equal to target S.
+ * <p>
+ * Example 1:
+ * Input: nums is [1, 1, 1, 1, 1], S is 3.
+ * Output: 5
+ * Explanation:
+ * <p>
+ * -1+1+1+1+1 = 3
+ * +1-1+1+1+1 = 3
+ * +1+1-1+1+1 = 3
+ * +1+1+1-1+1 = 3
+ * +1+1+1+1-1 = 3
+ * <p>
+ * There are 5 ways to assign symbols to make the sum of nums be target 3.
+ */
+public class TargetSum {
+
+    public static void main(String[] args) {
+        System.out.println(findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
+    }
+
+    /**
+     * Recursion with memorisation
+     * @param nums
+     * @param S
+     * @return
+     */
+    public static int findTargetSumWays(int[] nums, int S) {
+        int[][] memo = new int[nums.length][2001];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return calculate(nums, 0, 0, S, memo);
+    }
+
+    public static int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (memo[i][sum + 1000] != Integer.MIN_VALUE) {
+                return memo[i][sum + 1000];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int sub = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + 1000] = add + sub;
+            return memo[i][sum + 1000];
+        }
+    }
+}
