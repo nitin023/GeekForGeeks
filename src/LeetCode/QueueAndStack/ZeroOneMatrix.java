affed21a17a605783e3e3987172396a5dcb1c3b8
nitinFareye1 <nitin.khulbe@getfareye.com>
2020-04-26 17:49:12 +0000

    New solution for matrix zero one problem having easy DP approach added

--- src/LeetCode/QueueAndStack/ZeroOneMatrix.java
+++ src/LeetCode/QueueAndStack/ZeroOneMatrix.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.Integer.min;
+
 /**
  * 01 Matrix
  * Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
@@ -133,5 +135,54 @@ public class ZeroOneMatrix {
             }
         }
         return minAsPerRow;
+    }
+
+    /**
+     * DP Approach
+     * <p>
+     * Approach #3 DP Approach [Accepted]
+     * Intuition
+     * <p>
+     * The distance of a cell from 0 can be calculated if we know the nearest distance for all the neighbours,
+     * in which case the distance is minimum distance of any neightbour + 1. And, instantly, the word come to mind DP!!
+     * For each 1, the minimum path to 0 can be in any direction.
+     * So, we need to check all the 4 direction.
+     * In one iteration from top to bottom, we can check left and top directions, and we need another iteration from bottom to top to check for right and bottom direction.
+     * <p>
+     * Algorithm
+     * <p>
+     * Iterate the matrix from top to bottom-left to right:
+     * Update \text{dist}[i][j]=\min(\text{dist}[i][j],\min(\text{dist}[i][j-1],\text{dist}[i-1][j])+1)dist[i][j]=min(dist[i][j],min(dist[i][j−1],dist[i−1][j])+1)
+     * i.e., minimum of the current dist and distance from top or left neighbour +1,
+     * that would have been already calculated previously in the current iteration.
+     * Now, we need to do the back iteration in the similar manner: from bottom to top-right to left:
+     * Update \text{dist}[i][j]=\min(\text{dist}[i][j],\min(\text{dist}[i][j+1],\text{dist}[i+1][j])+1)dist[i][j]=min(dist[i][j],min(dist[i][j+1],dist[i+1][j])+1) i.e. minimum of current dist and distances calculated from bottom and right neighbours, that would be already available in current iteration.
+     */
+    public static int[][] updateMatrixDP(int[][] matrix) {
+        int[][] dist = new int[matrix.length][];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0)
+                    dist[i][j] = 0;
+                else {
+                    if (i > 0)
+                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
+                    if (j > 0)
+                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
+                }
+            }
+        }
+
+        //Second pass: check for bottom and right
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            for (int j = matrix[i].length - 1; j >= 0; j--) {
+                if (i < matrix.length - 1)
+                    dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
+                if (j < matrix[i].length - 1)
+                    dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
+            }
+        }
+
+        return dist;
     }
 }
