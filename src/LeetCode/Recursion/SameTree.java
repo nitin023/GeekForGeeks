d32e2b59c08b31e98406fbd566b192df5edf6566
nitinFareye1 <nitin.khulbe@getfareye.com>
2020-05-12 02:52:26 +0000

    Solved Same tree problem using slight tweaking of level order traversal to check for node values and structrural integrity

--- src/LeetCode/Recursion/SameTree.java
+++ src/LeetCode/Recursion/SameTree.java
@@ -2,33 +2,68 @@ package LeetCode.Recursion;
 
 import LeetCode.Easy.Tree.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Two trees are called same iff they are structurally same
- *
+ * <p>
  * Problem can be solved using recursion and iteration
  */
 public class SameTree {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
-        return helperRecursive(p,q);
+        return helperIteratively(p, q);
     }
 
-    private boolean helperRecursive(TreeNode p, TreeNode q)
-    {
+    private boolean helperRecursive(TreeNode p, TreeNode q) {
 
-        if(p == null && q == null)
-        {
+        if (p == null && q == null) {
             return true;
         }
-        if(p==null || q==null)
-        {
+        if (p == null || q == null) {
             return false;
         }
-        if(p.val != q.val)
-        {
+        if (p.val != q.val) {
             return false;
         }
-        return helperRecursive(p.left,q.left) && helperRecursive(p.right , q.right);
+        return helperRecursive(p.left, q.left) && helperRecursive(p.right, q.right);
+    }
+
+    private boolean checkIfNodeAreSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val;
+    }
+
+    private boolean helperIteratively(TreeNode p, TreeNode q) {
+        return levelOrderTraversal(p).equals(levelOrderTraversal(q));
+    }
+
+    private String levelOrderTraversal(TreeNode tree) {
+        if (tree == null) {
+            return "";
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(tree);
+
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            TreeNode currentNode = queue.poll();
+            if (currentNode == null) {
+                sb.append("N");
+            } else {
+                sb.append(currentNode.val);
+                queue.add(currentNode.left);
+                queue.add(currentNode.right);
+            }
+        }
+        return sb.toString();
     }
 }
