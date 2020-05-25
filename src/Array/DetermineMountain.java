7f3a0ca6aaa2099800195b534c7691bc4435af7a
nitin023 <khulbe.nitin@gmail.com>
2020-05-25 20:31:08 +0530

    Solved validating array mountain formation

---
+++ src/Array/DetermineMountain.java
@@ -0,0 +1,38 @@
+package Array;
+
+public class DetermineMountain {
+
+    public static void main(String[] args) {
+        System.out.println(validMountainArray(new int[]{1,2,3,5,7,9,10,12,14,15,16,18,14,13}));
+    }
+
+    public static boolean validMountainArray(int[] A) {
+        if (A == null || A.length < 3) {
+            return false;
+        }
+
+        int i;
+        boolean isMountainStricltyIncreasing = false;
+        for (i = 0; i + 1 < A.length; i++) {
+            if (i > 0 && A[i - 1] < A[i] && A[i] > A[i + 1]) {
+                isMountainStricltyIncreasing = true;
+                break;
+            } else if (A[i] > A[i + 1]) {
+                if (!isMountainStricltyIncreasing) {
+                    return false;
+                }
+            }
+        }
+
+        if (isMountainStricltyIncreasing) {
+            while (i + 1 < A.length) {
+                if (A[i + 1] >= A[i]) {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+        return false;
+    }
+}
